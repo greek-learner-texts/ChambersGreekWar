4878509907e5836d3486720222d80c337eef9c66
sarahmccuan <salinthesouth@gmail.com>
2023-09-19 10:39:43 -0500

    normalization try 2

--- scripts/normalize.py
+++ scripts/normalize.py
@@ -1,21 +1,26 @@
 import glob
 import sys
 import unicodedata
-from greek_normalisation.utils import convert_to_2019
+from greek_normalisation.utils import convert_to_2019, nfc
+from text_validator.main import validate
+from pathlib import Path 
 
 def normalize_file(filename):
     try: 
         with open(filename, 'r+', encoding='utf-8') as f:
             file_contents = f.read()
             f.seek(0)
-            f.write(convert_to_2019(file_contents))
+            f.write(nfc(convert_to_2019(file_contents)))
             f.truncate()
     except:
         print('Error: ' + str(sys.exc_info()[0]))
 
-# TO-DO - refactor this to pass in a directory from command line 
-files_dir = r'C:\Users\Sarah McCuan\Documents\projects\ChambersGreekWar\text\\'
-files_list = glob.glob(files_dir + '*.txt')
+# relative path logic
+chambers_path = Path(__file__).parent.parent
+files_dir = (chambers_path / 'text\\').resolve()
+files_list = glob.glob(str(files_dir) + '*.txt')
 
 for file in files_list:
     normalize_file(file)
+
+print(validate((chambers_path / 'text-validator.toml').resolve(), files_list))
